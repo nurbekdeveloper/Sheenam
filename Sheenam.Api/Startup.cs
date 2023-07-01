e47696a480179c8806163506ff6845413e2e26ac
nurbekdeveloper <nurbeknormatov_02@gmail.com>
2023-07-01 15:09:51 +0500

    Config:Register Broker

--- Sheenam.Api/Startup.cs
+++ Sheenam.Api/Startup.cs
@@ -36,7 +36,7 @@ namespace Sheenam.Api
 
             services.AddControllers();
             services.AddDbContext<StorageBroker>();
-            services.AddTransient<IStorageBroker , StorageBroker>();
+           
             services.AddSwaggerGen(options =>
 
                 options.SwaggerDoc(
