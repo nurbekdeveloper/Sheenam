9af386e854c265e42c2991b3450ef4d67300f42d
nurbekdeveloper <nurbeknormatov_02@gmail.com>
2024-03-14 14:19:46 +0500

    INFRA:Unit Test Project

---
+++ Sheenam.Api/Controllers/HomeController.cs
@@ -0,0 +1,14 @@
+﻿
+using Microsoft.AspNetCore.Mvc;
+using RESTFulSense.Controllers;
+
+namespace Sheenam.Api.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class HomeController : RESTFulController
+    {
+        [HttpGet]
+        public ActionResult<string> GetAction() => "hello Nurbek Prience is another castle ";
+    }
+}
