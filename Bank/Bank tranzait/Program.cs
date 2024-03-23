edb6512da20756fa3f48ef1ebfea49103c281388
nurbekdeveloper <nurbeknormatov_02@gmail.com>
2024-03-23 19:39:34 +0500

    INFRA: Build project

---
+++ Bank/Bank tranzait/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Bank_tranzait
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
